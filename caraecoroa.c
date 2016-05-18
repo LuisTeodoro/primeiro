765df4fc3fdbb55d6e88d2e59768e75419d9206a
LuisTeodoro <luisteodoro92@outlook.com>
2016-05-17 21:48:21 -0300

    Update caraecoroa.c

--- caraecoroa.c
+++ caraecoroa.c
@@ -2,16 +2,18 @@
 
 int main()
 {
-  int maria = 0,joao =0;
-  int jogada, i;
+int jogada, i;
   
-  scanf("%d", &jogada);
+  
   while(jogada != 0){
+    int maria = 0,joao =0;
+    
     for(i = 0; i < jogadas, i++){
       scanf("%d", &jogada);
       if(jogada == 0) maria++;
       else joao++;
     }
     printf("maria ganhou %d e jao ganhou %d", maria, joao);
+    scanf("%d", &jogada);
   }
 }
