44f6df2f11300c92cedb237981f99548ac8a8b5d
LuisTeodoro <luisteodoro92@outlook.com>
2016-05-17 21:46:47 -0300

    Create caraecoroa.c

---
+++ caraecoroa.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main()
+{
+  int maria = 0,joao =0;
+  int jogada, i;
+  
+  scanf("%d", &jogada);
+  while(jogada != 0){
+    for(i = 0; i < jogadas, i++){
+      scanf("%d", &jogada);
+      if(jogada == 0) maria++;
+      else joao++;
+    }
+    printf("maria ganhou %d e jao ganhou %d", maria, joao);
+  }
+}
